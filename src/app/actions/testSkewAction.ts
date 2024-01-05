8d15df2257c30579de291b06919218b47ca8308b
Gabriel Bianchi <gdbianchii@gmail.com>
2024-01-05 14:14:56 -0300

    use unstable cache

--- src/app/actions/testSkewAction.ts
+++ src/app/actions/testSkewAction.ts
@@ -1,5 +1,11 @@
 "use server";
 
+import { unstable_cache } from "next/cache";
+
+const getId = unstable_cache(async () => {
+  return Math.random();
+});
+
 export const testSkewAction = async () => {
-  return process.env.VERCEL_DEPLOYMENT_ID;
+  return await getId();
 };
